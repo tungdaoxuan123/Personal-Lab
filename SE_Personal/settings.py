4d8e98ce6aaaa5e3a34d3c35e84567e3d0d3c315
tungdaoxuan <explodingww@gmail.com>
2022-04-24 11:07:38 +0700

    after inherritant and templates

--- SE_Personal/settings.py
+++ SE_Personal/settings.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'SE_Personal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.dirname(os.path.dirname(__file__)), 'accounts/templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
