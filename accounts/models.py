040f08ba0b34a3e3d89dd4afd0190a28ee12631e
tungdaoxuan <explodingww@gmail.com>
2022-04-24 21:38:04 +0700

    userrole & user profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Customer(models.Model):
+	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
 	phone = models.CharField(max_length=200, null=True)
 	email = models.CharField(max_length=200, null=True)
@@ -49,3 +51,5 @@ class Order(models.Model):
 
 	def __str__(self):
 		return self.product.name
+
+
